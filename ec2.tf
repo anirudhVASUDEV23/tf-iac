93d677a99a836fc98b276bea0b6a307630fecad5
anirudhVASUDEV23 <anirudh.vasudev.kesana@gmail.com>
2026-03-28 15:43:08 +0530

    ec2-provisioning done

---
+++ ec2.tf
@@ -0,0 +1,74 @@
+#key-pair for login
+resource aws_key_pair my_key {
+    key_name="terra-key-ec2"
+    public_key = file("terra-key-ec2.pub")
+}
+
+#vpc & security group
+resource aws_default_vpc default {
+    tags={
+        Name="Default VPC"
+    }
+}
+
+resource aws_security_group my_security_group {
+    name = "automate-sg"
+    description = "This will add a TF generated Security group"
+    vpc_id =aws_default_vpc.default.id #interpolation
+
+    #inbound rules
+    ingress{
+       from_port = 22
+       to_port = 22
+       protocol = "tcp"
+       cidr_blocks = ["0.0.0.0/0"] ##ssh from anywhere
+       description = "SSH Open"
+    }
+
+    ingress{
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "HTTP OPEN"
+    }
+
+    ingress{
+        from_port = 8000
+        to_port = 8000
+        cidr_blocks = ["0.0.0.0/0"]
+        protocol = "tcp"
+        description = "Exposing An Application at this port"
+    }
+
+    #outbound rules
+    egress{
+       from_port = 0
+       to_port = 0
+       protocol = "-1"
+       cidr_blocks = ["0.0.0.0/0"]
+       description = "All Access Open"
+    }
+
+    tags = {
+    Name = "automate-sg"
+  }
+}
+
+#ec2 Instance
+
+resource aws_instance my_instance {
+    key_name = aws_key_pair.my_key.key_name
+    security_groups = [aws_security_group.my_security_group.name]
+    instance_type ="t2.micro"
+    ami= "ami-07062e2a343acc423"
+
+    root_block_device{
+        volume_size = 8
+        volume_type = "gp3"
+    }
+
+    tags={
+        Name="TF EC2"
+    }
+}
