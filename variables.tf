59fecd4cf58c67f133561ff28ce17c39f416b758
anirudhVASUDEV23 <anirudh.vasudev.kesana@gmail.com>
2026-03-28 16:12:06 +0530

    added variables and outputs tf files

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ec2_instance_type"{
+    default="t2.micro"
+    type=string
+}
+
+
+variable "ec2_root_storage_size" {
+    default=9
+    type=number
+}
+
+variable "ec2_ami_id" {
+    default="ami-07062e2a343acc423"
+    type=string 
+}
